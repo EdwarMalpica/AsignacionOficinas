68dde8b739a3003a4b27a922f19ba639de65c3bc
heiiit <titav5261@gmail.com>
2021-09-28 22:36:34 -0500

    inicio crud

---
+++ Ofi.App/Ofi.App.Persistencia/AppRepositorio/IRepositorioSecretaria.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using Ofi.App.Persistencia;
+using Ofi.App.Dominio;
+
+
+namespace Ofi.App.Persistencia
+{
+    
+public interface IRepositorioSecretaria
+{
+    //GetAllSecretaria
+
+    IEnumerable <Secretaria> GetAllSecretaria();
+
+    //GetSecretaria
+    Secretaria GetSecretaria(int idSecretaria);
+
+    //AddSecretaria
+    Secretaria AddSecretaria(Secretaria secretaria);
+
+    //UpdateSecretaria
+    Secretaria UpdateSecretaria(Secretaria secretaria);
+
+    //DeleteSecretaria
+    void DeleteSecretaria(int idSecretaria);
+
+
+
+}
