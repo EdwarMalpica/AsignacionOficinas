0bfc1b814c701c6dec8e9e4f22e4172317a032db
Edwar Malpica <edwar.malpica@uptc.edu.co>
2021-09-29 22:39:51 -0500

    Update Ingreso.cs
    Añadir id

--- Ofi.App/Ofi.App.Dominio/entidades/Ingreso.cs
+++ Ofi.App/Ofi.App.Dominio/entidades/Ingreso.cs
@@ -4,6 +4,7 @@ namespace Ofi.App.Dominio
 {
     public class Ingreso
     { 
+        public int id{get;set;}
         public DateTime horaIngreso{ get; set; }
         public Persona persona { get; set; }
         public Oficina oficina { get; set; }
