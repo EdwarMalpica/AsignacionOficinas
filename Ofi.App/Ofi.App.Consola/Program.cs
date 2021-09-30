501594c0d15150e6daa316634747d70de3a6b5c6
heiiit <titav5261@gmail.com>
2021-09-29 22:58:14 -0500

    correccion crud1

--- Ofi.App/Ofi.App.Consola/Program.cs
+++ Ofi.App/Ofi.App.Consola/Program.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using Ofi.App.Dominio;
 using Ofi.App.Persistencia;
+using System.Collections.Generic;
 
 namespace Ofi.App.Consola
 {
     class Program
     {
+        private static IRepositorioSecretaria _repoSecretaria= new RepositorioSecretaria(new Persistencia.AppContext());
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             AdicionarSecretaria();
             //ActualizarSecretaria(1);
-            EncontrarSecretarias(1);
+            EncontrarSecretaria(1);
             EncontrarSecretarias();
             //EliminarSecretaria(1);
         }
@@ -30,18 +32,18 @@ namespace Ofi.App.Consola
         }
 
         //ActualizarSecretaria
-        private static void ActualizarSecretaria (int idSecretaria)
-        {
-                var secretaria = new Secretaria
-                {
-                    id=idSecretaria
-                };
+        //private static void ActualizarSecretaria (int idSecretaria)
+        //{
+        //        var secretaria = new Secretaria
+        //        {
+        //            id=idSecretaria
+        //        };
 
-            Secretaria SecretariaActualizada =_repoSecretaria.UpdateSecretaria(secretaria);
-            if (SecretariaActualizada != null)
-                Console.WriteLine("se actualizó la secretaria");
+        //    Secretaria SecretariaActualizada =_repoSecretaria.UpdateSecretaria(secretaria);
+        //    if (SecretariaActualizada != null)
+        //        Console.WriteLine("se actualizó la secretaria");
 
-        }
+        //}
 
         //EncontrarSecretaria
         private static void EncontrarSecretaria (int idSecretaria)
