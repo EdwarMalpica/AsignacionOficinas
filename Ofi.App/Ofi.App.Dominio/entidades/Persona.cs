eca29cf3cfca902f40cdcf09c3e57748a38dd579
Edwar Malpica <edwar.malpica@uptc.edu.co>
2021-10-02 17:03:55 -0500

    Implementacion del paquete Frontend

--- Ofi.App/Ofi.App.Dominio/entidades/Persona.cs
+++ Ofi.App/Ofi.App.Dominio/entidades/Persona.cs
@@ -5,7 +5,16 @@ namespace Ofi.App.Dominio
     public class Persona
     {
 
-        public int id { get; set; }
+        public Persona(int id, string nombre, string apellido, int edad, EstadoCovid estadoCovid) 
+        {
+                this.idPersona = id;
+                this.nombre = nombre;
+                this.apellido = apellido;
+                this.edad = edad;
+                this.estadoCovid = estadoCovid;
+               
+        }
+        public int idPersona { get; set; }
         public string nombre { get; set; }
         public string apellido { get; set; }
         public int edad { get; set; }
--- Ofi.App/Ofi.App.Dominio/entidades/PersonalDeAseo.cs
+++ Ofi.App/Ofi.App.Dominio/entidades/PersonalDeAseo.cs
@@ -3,16 +3,15 @@ using System;
 namespace Ofi.App.Dominio
 {
     public class PersonalDeAseo : Persona
-
     {
-        public PersonalDeAseo(int id, Turno turno, Secretaria unidadServicio) 
+        public PersonalDeAseo(int idPersona, string nombre, string apellido, int edad, EstadoCovid estadoCovid,int id, Turno turno, Secretaria unidadServicio) : base(idPersona, nombre, apellido, edad, estadoCovid)
         {
             this.id = id;
-                this.turno = turno;
-                this.unidadServicio = unidadServicio;
-               
+            this.turno = turno;
+            this.unidadServicio = unidadServicio;
         }
-                public int id { get; set; }
+
+        public int id { get; set; }
         public Turno turno { get; set; }
         public Secretaria unidadServicio { get; set; }
     }
