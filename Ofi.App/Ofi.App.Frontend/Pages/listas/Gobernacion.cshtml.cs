a91f171327d844cb352578a263870a8dc3f3be8c
Edwin <edwingonzalez.0789@gmail.com>
2021-10-09 22:49:07 -0500

    listasgobernacionesfrontend

--- Ofi.App/Ofi.App.Frontend/Pages/listas/Gobernacion.cshtml.cs
+++ Ofi.App/Ofi.App.Frontend/Pages/listas/Gobernacion.cshtml.cs
@@ -4,13 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Ofi.App.Dominio;
+using Ofi.App.Persistencia;
+
 
 namespace Ofi.App.Frontend.Pages
 {
     public class GobernacionModel : PageModel
     {
+
+        
+        private static IRepositorioGobernacion _repoGobernacion = new RepositorioGobernacion(new Persistencia.AppContext());
+        public IEnumerable<Gobernacion> gobernaciones {get;set;}
+
+
         public void OnGet()
         {
+
+            gobernaciones=_repoGobernacion.GetAllGobernaciones();
         }
     }
 }
